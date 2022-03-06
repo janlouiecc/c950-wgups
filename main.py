a6af112285f545994606aeb305adc304ddd6e6ad
Jan Louie Castro <70475659+janlouiecc@users.noreply.github.com>
2022-03-05 20:11:04 -0700

    changed code
    added distances data

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import csv
 from hash import ChainingHashTable
-from package import Package
+from package import *
+import csv
 
 
 # This function loads a hash table with the package data
-def load_package_data(file_name):
+def load_package_data(file_name, table):
     with open(file_name) as package_data:
         package_data = csv.reader(package_data, delimiter=',')
         for package in package_data:
@@ -22,11 +22,30 @@ def load_package_data(file_name):
 
             pkg = Package(package_id, address, city, state, zip_code, deadline, mass_in_kg, notes)
 
-            packages_table.insert(package_id, pkg)
+            table.insert(package_id, pkg)
 
 
+# This function loads the addresses into a distance data list
+def load_distance_data(file_name, table):
+    with open(file_name) as distance_data:
+        distance_data = csv.reader(distance_data, delimiter=',')
+        for data in distance_data:
+            table.append(data)
+
+    for i in range(len(table)):
+        for j in range(len(table) + 2):
+            if table[i][j] == '':
+                table[i][j] = table[j-2][i+2]
+
+
+# Creates a hash table for the packages
 packages_table = ChainingHashTable()
-load_package_data('packages.csv')
 
-# for i in range(40):
-#     print(packages_table.search(i + 1), "\n")
+# Loads the packages into the packages hash table
+load_package_data('packages.csv', packages_table)
+
+# Creates a list to store distances between addresses
+distance_data_list = []
+
+# Loads the distance data into a list
+load_distance_data('distances.csv', distance_data_list)
