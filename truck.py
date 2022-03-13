b46341eb816d47203e145e13ddef7c3abecc5a34
Jan Louie Castro <70475659+janlouiecc@users.noreply.github.com>
2022-03-13 17:15:53 -0600

    currently working on truck class

--- truck.py
+++ truck.py
@@ -1,3 +1,19 @@
 
+
 class Truck:
-    pass
+
+    def __init__(self, packages):
+        self.packages = packages
+
+# def load_truck_packages():
+#     for _ in range(len(packages_table.table[0])):
+#         truck_a.append(packages_table.table[0][_])
+#
+#     print(truck_a[0][1].lookup("address"))
+#     print(truck_a)
+
+# truck1 = []
+# truck2 = []
+# truck3 = []
+#
+# load_truck_packages(truck1, truck2, truck3)
