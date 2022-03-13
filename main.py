82cd97a85ebc33a5b88eb0bb1f801eee00bf077a
Jan Louie Castro <70475659+janlouiecc@users.noreply.github.com>
2022-03-13 17:15:15 -0600

    cleaned up some code and moved load package func

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
 from hash import ChainingHashTable
 from package import *
+# from truck import *
 import csv
 
 
-# This function loads a hash table with the package data
-def load_package_data(file_name, table):
-    with open(file_name) as package_data:
-        package_data = csv.reader(package_data, delimiter=',')
-        for package in package_data:
-            package_id = int(package[0])
-            address = package[1]
-            city = package[2]
-            state = package[3]
-            zip_code = package[4]
-            deadline = package[5]
-            mass_in_kg = float(package[6])
-            if package[7] == "":
-                notes = None
-            else:
-                notes = package[7]
-
-            pkg = Package(package_id, address, city, state, zip_code, deadline, mass_in_kg, notes)
-
-            table.insert(package_id, pkg)
-
-
 # This function loads the addresses and their distances into a distance data list
-def load_distance_data(file_name, table):
+def load_distance_data(file_name):
+    table = []
     with open(file_name) as distance_data:
         distance_data = csv.reader(distance_data, delimiter=',')
         for data in distance_data:
@@ -38,13 +18,18 @@ def load_distance_data(file_name, table):
             if table[i][j] == '':
                 table[i][j] = table[j - 3][i + 3]
 
+    return table
+
 
 # This function loads the addresses into an address data list , delimiter=','
-def load_address_data(file_name, table):
+def load_address_data(file_name):
+    table = []
     with open(file_name) as address_data:
         address_data = csv.reader(address_data, delimiter=',')
         for data in address_data:
             table.append(data)
+
+    return table
 
 
 # This function finds the distances between two addresses
@@ -68,41 +53,14 @@ def distance_between(address1, address2):
             continue
 
 
-def minimum_distance_from(from_address):  # truck_packages
-    i = 0
-    j = 3
-    for _ in distance_data_list:
-        if distance_data_list[i][1] == from_address:
-            min_dist = distance_data_list[i][j]
-            for _ in range(len(distance_data_list[i])):
-
-                if min_dist <= distance_data_list[i][j]:  # figure this out, how to keep  it in range
-                    continue
-                else:
-                    min_dist = distance_data_list[i][j]
-        i += 1
-
-
 # Creates a hash table for the packages
 packages_table = ChainingHashTable()
 
-# Loads the packages into the packages hash table
-load_package_data('packages.csv', packages_table)
+# Loads the packages into the packages table
+load_package_data('packages.csv', ChainingHashTable())
 
-# Creates a list to store distances between addresses
-distance_data_list = []
+# Creates a list to store distances between addresses then calls the function to load the distance data into the list
+distance_data_list = load_distance_data('distances.csv')
 
-# Loads the distance data into a list
-load_distance_data('distances.csv', distance_data_list)
-
-# Creates a list to store distances between addresses
-address_data_list = []
-
-# Loads the distance data into a list
-load_address_data('addresses.csv', address_data_list)
-
-truck1 = []
-truck2 = []
-truck3 = []
-
-minimum_distance_from("195 W Oakland Ave")
+# Creates a list to store the addresses in the list then calls the function to load the addresses
+address_data_list = load_address_data('addresses.csv')
