ed13bd35e535b768cec1c6a06a4bc1aa2c4bcbef
Jan Louie Castro <70475659+janlouiecc@users.noreply.github.com>
2022-03-23 22:01:35 -0600

    minor tweaks

--- truck.py
+++ truck.py
@@ -1,27 +1,27 @@
 
 # This function will load the trucks with packages along with their associated constraints
 def load_truck_packages(truck1, truck2, truck3, delayed, packages):
-    for i in range(1, 41):
-        if packages.search(i).notes == "Must be delivered with 15, 19" or \
-                packages.search(i).notes == "Must be delivered with 13, 19" or \
-                packages.search(i).notes == "Must be delivered with 13, 15" or \
-                packages.search(i).package_id == 13 or packages.search(i).package_id == 15 or \
-                packages.search(i).package_id == 19:
-            truck1.append(packages.search(i))
-        elif packages.search(i).deadline == "EOD" and packages.search(i).notes is None:
+    for i in range(40):
+        if packages.search(i + 1).notes == "Must be delivered with 15, 19" or \
+                packages.search(i + 1).notes == "Must be delivered with 13, 19" or \
+                packages.search(i + 1).notes == "Must be delivered with 13, 15" or \
+                packages.search(i + 1).package_id == 13 or packages.search(i + 1).package_id == 15 or \
+                packages.search(i + 1).package_id == 19:
+            truck1.append(packages.search(i + 1))
+        elif packages.search(i + 1).deadline == "EOD" and packages.search(i + 1).notes is None:
             if len(truck2) > 10:
-                truck3.append(packages.search(i))
+                truck3.append(packages.search(i + 1))
             else:
-                truck2.append(packages.search(i))
-        elif packages.search(i).deadline != "EOD":
-            if packages.search(i).notes == "Delayed on flight---will not arrive to depot until 9:05 am":
-                delayed.append(packages.search(i))
+                truck2.append(packages.search(i + 1))
+        elif packages.search(i + 1).deadline != "EOD":
+            if packages.search(i + 1).notes == "Delayed on flight---will not arrive to depot until 9:05 am":
+                delayed.append(packages.search(i + 1))
             else:
-                truck1.append(packages.search(i))
-        elif packages.search(i).notes == "Can only be on truck 2":
-            truck2.append(packages.search(i))
+                truck1.append(packages.search(i + 1))
+        elif packages.search(i + 1).notes == "Can only be on truck 2":
+            truck2.append(packages.search(i + 1))
         else:
-            truck3.append(packages.search(i))
+            truck3.append(packages.search(i + 1))
 
 
 class Truck:
